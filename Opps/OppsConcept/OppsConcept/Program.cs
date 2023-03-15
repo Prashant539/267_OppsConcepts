d6cdc7e9958eefc464f0799ce3b918bdca5532f2
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-15 10:38:27 +0530

    [Prashant]Add: Adding Encapsulation

---
+++ Opps/OppsConcept/OppsConcept/Program.cs
@@ -0,0 +1,28 @@
+﻿using OppsConcept.Encapsulation;
+using System;
+namespace OppsConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Opps Concept");
+            Console.WriteLine("Enter the Options: ");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch(options)
+            {
+                case 1:
+                    Person person = new Person();
+                    Console.WriteLine("Enter the Name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter the Age: ");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    person.setName(name);
+                    person.getName(name);
+                    person.setAge(age);
+                    person.getAge(age);
+                    break;
+            }
+        }
+    }
+}
