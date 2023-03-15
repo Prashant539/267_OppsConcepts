2cac20085bd291ba6d0e205a2dbef3e304e5805b
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-15 10:55:42 +0530

    [Prashant]Add: Adding Abstraction

--- Opps/OppsConcept/OppsConcept/Program.cs
+++ Opps/OppsConcept/OppsConcept/Program.cs
@@ -1,4 +1,5 @@
-﻿using OppsConcept.Encapsulation;
+﻿using OppsConcept.Abstraction;
+using OppsConcept.Encapsulation;
 using System;
 namespace OppsConcept
 {
@@ -8,6 +9,7 @@ namespace OppsConcept
         {
             Console.WriteLine("Welcome to Opps Concept");
             Console.WriteLine("Enter the Options: ");
+            Console.WriteLine("1:Encapsulation\n2:Abstraction");
             int options = Convert.ToInt32(Console.ReadLine());
             switch(options)
             {
@@ -21,6 +23,13 @@ namespace OppsConcept
                     person.getName(name);
                     person.setAge(age);
                     person.getAge(age);
+                    break;
+                case 2:
+                    AbsChild p = new AbsChild();
+                    p.Add(10, 20);
+                    p.Sub(20, 10);
+                    p.Mul(10, 10);
+                    p.Div(20, 10);
                     break;
             }
         }
