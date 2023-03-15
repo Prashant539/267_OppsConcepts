d7018da183dc19e2a7294c099114ccb696bdedde
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-15 22:28:18 +0530

    [Prashant]Add: Showing class and object concept

--- Opps/OppsConcept/OppsConcept/Program.cs
+++ Opps/OppsConcept/OppsConcept/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OppsConcept.Abstraction;
 using OppsConcept.Encapsulation;
+using OppsConcept.ObjectAndClass;
 using OppsConcept.Polymorphism;
 using System;
 namespace OppsConcept
@@ -10,7 +11,7 @@ namespace OppsConcept
         {
             Console.WriteLine("Welcome to Opps Concept");
             Console.WriteLine("Enter the Options: ");
-            Console.WriteLine("1:Encapsulation\n2:Abstraction\n3:Inheritance\n4:Polymorphism");
+            Console.WriteLine("1:Encapsulation\n2:Abstraction\n3:Inheritance\n4:Polymorphism\n5:ObjectAndClass");
             int options = Convert.ToInt32(Console.ReadLine());
             switch(options)
             {
@@ -46,6 +47,16 @@ namespace OppsConcept
                    // loadChild.Show();
                     loadChild.Test();
                     break;
+                case 5:
+                    Student student = new Student(); //creating an object of Student  
+                    student.id = 101;
+                    student.name = "Prashant";
+                    Console.WriteLine(student.id);
+                    Console.WriteLine(student.name);
+                    break;
+
+
+                    
                 
             }
         }
